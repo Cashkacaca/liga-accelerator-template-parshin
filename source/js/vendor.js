f033a2667c690675c1b7b2da5cbf29d051f3a2d4
Александр Паршин <cashkacaca@gmail.com>
2023-03-20 23:09:35 +0300

    Выполнил аккордеон

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,12 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+
+// аккордеоен
+import {initAccordions} from './vendor/init-accordion';
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initAccordions();
+  });
+});
